ed41a6564254ffbd6a3cd8e6a468da80078d6cc1
Khang Le Truong <khang.letruong@pexraytech.com>
2020-10-25 11:51:26 +0700

    Standardize fetching method

--- package/flir-lepton-tools/lepton-spidev-capture/lepton-spidev-capture.mk
+++ package/flir-lepton-tools/lepton-spidev-capture/lepton-spidev-capture.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 LEPTON_SPIDEV_CAPTURE_VERSION = v1.0.0
-LEPTON_SPIDEV_CAPTURE_SITE = https://github.com/l4es/lepton-spidev-capture.git
-LEPTON_SPIDEV_CAPTURE_SITE_METHOD = git
+LEPTON_SPIDEV_CAPTURE_SITE = $(call github,l4es,lepton-spidev-capture,$(LEPTON_SPIDEV_CAPTURE_VERSION))
 LEPTON_SPIDEV_CAPTURE_INSTALL_STAGING = NO
 
 define LEPTON_SPIDEV_CAPTURE_BUILD_CMDS
